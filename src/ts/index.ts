1828b2f7556f8b6cccfa4deeabf2afc4dca1eb3d
yjkim <yjkim@aimmed.com>
2022-02-03 17:01:10 +0900

    first

---
+++ src/ts/index.ts
@@ -0,0 +1,8 @@
+let first:string = 'test';
+console.log(first);
+
+let test_id:number = 121212;
+let test_name:string = 'j';
+
+console.log('test_id:', test_id);
+console.log('test_name:', test_name);
